d2dbe7dbd6ac540ddba4cbb81fc3c5026407968b
ayulestari10 <lestariayu669@gmail.com>
2017-01-09 23:20:02 +0700

    versi 0.0.89

--- application/views/home.php
+++ application/views/home.php
@@ -205,7 +205,7 @@
             width: 100%; height: 100%;
         }
         #inti{
-            margin-top: -10%;
+            margin-top: -50%;
             background: url(<?= base_url('assets/img/header.png') ?>) no-repeat bottom center scroll;
             background-size: contain;
             height: 900px;
