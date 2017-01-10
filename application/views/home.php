840409708d2100a768e8fa53581381a7a938d679
ayulestari10 <lestariayu669@gmail.com>
2017-01-11 05:35:51 +0700

    versi 0.26.0

--- application/views/home.php
+++ application/views/home.php
@@ -168,7 +168,7 @@
 
     <style type="text/css">
         #coming img{
-            width: 100%; height: 630px;
+            width: 100%; height: 640px;
             /*margin-bottom: -2%;*/
         }
     </style>
