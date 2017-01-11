8bfea0ac56783bce37ce161f5d5140a26592f476
ayulestari10 <lestariayu669@gmail.com>
2017-01-11 21:28:10 +0700

    versi 0.32.0

--- application/controllers/Peserta.php
+++ application/controllers/Peserta.php
@@ -21,9 +21,23 @@ class Peserta extends CI_Controller{
 	}
 
 	function daftar(){
-		$nim = $this->session->userdata('nim');
+		$nim 		= $this->session->userdata('nim');
+		
+		$nama		= $this->input->post('nama');
+		$jurusan 	= $this->input->post('jurusan');
+		$angkatan 	= $this->input->post('angkatan');
+		$ipk 		= $this->input->post('ipk');
+		$alamat 	= $this->input->post('alamat');
+		$tempat 	= $this->input->post('tempat');
+		$ttl 		= $this->input->post('ttl');
+		$no_hp 		= $this->input->post('no_hp');
+		$dinas1 	= $this->input->post('dinas1');
+		$dinas2		= $this->input->post('dinas2');
+		$alasan1 	= $this->input->post('alasan1');
+		$alasan2 	= $this->input->post('alasan2');
+		$alasan_bem = $this->input->post('alasan_bem');
+		$id_line 	= $this->input->post('id_line');
 
-		$ipk = $this->input->post('ipk');
 
 		if($ipk > 4){
 			$this->session->set_flashdata('msg', '<div class="alert alert-danger">IPK yang anda inputkan tidak boleh lebih dari 4! Isi kembali IPK yang sesuai!</div>');
@@ -32,27 +46,33 @@ class Peserta extends CI_Controller{
 		}
 
 		if($this->input->post('simpan')){
-			$input = array(
-				'nama'		=> $this->input->post('nama'),
-				'jurusan'	=> $this->input->post('jurusan'),
-				'angkatan'	=> $this->input->post('angkatan'),
-				'ipk'		=> $this->input->post('ipk'),
-				'alamat'	=> $this->input->post('alamat'),
-				'tempat'	=> $this->input->post('tempat'),
-				'ttl'		=> $this->input->post('ttl'),
-				'no_hp'		=> $this->input->post('no_hp'),
-				'dinas1'	=> $this->input->post('dinas1'),
-				'alasan1'	=> $this->input->post('alasan1'),
-				'dinas2'	=> $this->input->post('dinas2'),
-				'alasan2'	=> $this->input->post('alasan2'),
-				'id_line'	=> $this->input->post('id_line'),
-				'alasan_bem'=> $this->input->post('alasan_bem'),
-			);
+			if(isset($nama, $jurusan, $angkatan, $ipk, $alamat, $tempat, $ttl, $no_hp, $dinas1, $dinas2, $alasan1, $alasan2, $alasan_bem, $id_line)){
+					$input = array(
+					'nama'		=> $nama,
+					'jurusan'	=> $jurusan,
+					'angkatan'	=> $angkatan,
+					'ipk'		=> $ipk,
+					'alamat'	=> $alamat,
+					'tempat'	=> $tempat,
+					'ttl'		=> $ttl,
+					'no_hp'		=> $no_hp,
+					'dinas1'	=> $dinas1,
+					'alasan1'	=> $alasan1,
+					'dinas2'	=> $dinas2,
+					'alasan2'	=> $alasan2,
+					'id_line'	=> $id_line,
+					'alasan_bem'=> $alasan_bem,
+				);
 
-			$this->Peserta_model->update($nim, $input);
-			$this->session->set_flashdata('msg', '<div class="alert alert-success">Data berhasil disimpan! Cetak ID CARD!</div>');
-			redirect('Peserta');
-			exit;
+				$this->Peserta_model->update($nim, $input);
+				$this->session->set_flashdata('msg', '<div class="alert alert-success">Data berhasil disimpan! Cetak ID CARD!</div>');
+				redirect('Peserta');
+				exit;
+			} else {
+				$this->session->set_flashdata('msg', '<div class="alert alert-danger">Data tidak berhasil disimpan. Lengkap data Anda.!</div>');
+				redirect('Peserta');
+				exit;
+			}
 		}
 		$data = array(
 			'title'		=> 'Daftar',
