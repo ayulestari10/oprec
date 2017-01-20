0480459c0f91cbd378108fd3c58e7d915c5e033c
ayulestari10 <lestariayu669@gmail.com>
2017-01-20 11:37:36 +0700

    selesai

--- application/views/home.php
+++ application/views/home.php
@@ -196,7 +196,7 @@
             </div> -->
             <div class="row" id="coming">
                 <div class="col-md-8 col-sm-8 col-lg-offset-2">
-                    <img src="<?= base_url('assets/img/pamflet.jpg') ?>">
+                    <img src="<?= base_url('assets/img/oprec.png') ?>">
                 </div>
             </div>
             <!-- <div class="container" style="margin-top: -10%;">
